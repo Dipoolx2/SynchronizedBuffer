cd993b79a822c9440398b1fbae29c6f54b23778e
Dipoolx2 <zygrobusiness@gmail.com>
2025-10-08 18:44:39 +0200

    Made logger thread safe

--- main.cpp
+++ main.cpp
@@ -22,27 +22,62 @@ class Logger
 {
   private:
   vector<string> logs;
+  mutex log_mtx;
+
+  // Internal helper (assumes mutex is already locked)
+  bool _read_at_nolock(const int index, std::string& message) {
+      if (index < 0 || index >= static_cast<int>(logs.size()))
+          return false;
+
+      message = logs.at(index);
+      return true;
+  }
   
   public:
   void log(const string& message) {
-    logs.push_back(message);
+    log_mtx.lock();
+    try {
+      logs.push_back(message);
+      log_mtx.unlock();
+    } catch (exception& ex) {
+      cerr << "Caught exception: " << ex.what() << endl;
+      log_mtx.unlock();
+    }
   }
 
   // Returns whether retrieving the last message went okay. If it went ok, the message is stored to the given message reference.
   bool read_last(string& message) {
-    const int last_index = logs.size() - 1;
+    log_mtx.lock();
+    try {
+      const int last_index = logs.size() - 1;
+      bool ret = _read_at_nolock(last_index, message);
+      log_mtx.unlock();
 
-    return read_at(last_index, message);
-  }
+      return ret;
+    } catch (exception& ex) {
+      cerr << "Caught exception: " << ex.what() << endl;
+      log_mtx.unlock();
 
-  bool read_at(const int index, string& message) {
-    if (index < 0 || index >= logs.size()) {
       return false;
     }
 
-    message = logs.at( index );
-    return true;
   }
+
+  bool read_at(const int index, string& message) {
+    log_mtx.lock();
+    try {
+      bool ret = _read_at_nolock(index, message);
+      log_mtx.unlock();
+
+      return ret;
+    } catch (exception& ex) {
+      cerr << "Caught exception: " << ex.what() << endl;
+      log_mtx.unlock();
+
+      return false;
+    }
+  }
+
 };
 
 int main(int argc, char *argv[])

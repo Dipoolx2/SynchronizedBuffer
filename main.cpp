be5548e9abcf782e87a4754ff595f7fe8f591b85
Dipoolx2 <zygrobusiness@gmail.com>
2025-10-08 22:18:08 +0200

    Allow multiple simultaneous readers, added a test

--- main.cpp
+++ main.cpp
@@ -15,17 +15,20 @@
 #include <thread>
 #include <vector>
 
-// although it is good habit, you don't have to type 'std::' before many objects by including this line
+// although it is good habit, you don't have to type '' before many objects by including this line
 using namespace std;
 
 class Logger
 {
 private:
   vector<string> log_vector;
-  mutex log_mtx;
+
+  mutex read_mtx;
+  mutex write_mtx;
+  int reader_count = 0;
 
   // Internal helper (assumes mutex is already locked)
-  bool _read_at_nolock(const int index, std::string &message)
+  bool read_at_nolock(const int index, string &message)
   {
     if (index < 0 || index >= static_cast<int>(log_vector.size()))
       return false;
@@ -34,114 +37,176 @@ private:
     return true;
   }
 
+  // Used if a reader has started reading. In this time no one can write.
+  void lock_read()
+  {
+    read_mtx.lock();
+    reader_count++;
+    if (reader_count == 1)
+    {
+      // Lock writers, at least one reader is active.
+      write_mtx.lock();
+    }
+
+    read_mtx.unlock();
+  }
+
+  // Used if a reader has stopped reading. If there are no readers left one can start writing again.
+  void unlock_read()
+  {
+    read_mtx.lock();
+    reader_count--;
+    if (reader_count == 0)
+    {
+      // Unlock writers, no more readers are active.
+      write_mtx.unlock();
+    }
+    read_mtx.unlock();
+  }
+
 public:
   void log(const string &message)
   {
-    log_mtx.lock();
+    write_mtx.lock();
 
     try
     {
       log_vector.push_back(message);
 
-      log_mtx.unlock();
+      write_mtx.unlock();
     }
     catch (exception &ex)
     {
       cerr << "Caught exception: " << ex.what() << endl;
 
-      log_mtx.unlock();
+      write_mtx.unlock();
     }
   }
 
   // Returns whether retrieving the last message went okay. If it went ok, the message is stored to the given message reference.
   bool read_last(string &message)
   {
-    log_mtx.lock();
+    lock_read();
+    bool ret = false;
 
     try
     {
       const int last_index = log_vector.size() - 1;
-      bool ret = _read_at_nolock(last_index, message);
-
-      log_mtx.unlock();
-      return ret;
+      ret = read_at_nolock(last_index, message);
+      unlock_read();
     }
     catch (exception &ex)
     {
       cerr << "Caught exception: " << ex.what() << endl;
 
-      log_mtx.unlock();
+      unlock_read();
       return false;
-    }
-  }
-
-  bool read_at(const int index, string &message)
-  {
-    log_mtx.lock();
-
-    try
-    {
-      bool ret = _read_at_nolock(index, message);
-      log_mtx.unlock();
-
-      return ret;
-    }
-    catch (exception &ex)
-    {
-      cerr << "Caught exception: " << ex.what() << endl;
-      log_mtx.unlock();
-
-      return false;
-    }
-  }
-
-  string read_all()
-  {
-    log_mtx.lock();
-
-    string ret = "";
-    try
-    {
-      for (string log : log_vector)
-      {
-        ret = ret + log + "\n";
-      }
-      log_mtx.unlock();
-    }
-    catch (const exception &e)
-    {
-      cerr << "Caught exception: " << e.what() << endl;
-      log_mtx.unlock();
     }
 
     return ret;
   }
 
-  void clear()
+  bool read_at(const int index, string &message)
   {
-    // clear the logger
-    log_mtx.lock();
+    lock_read();
+    bool ret = false;
+
     try
     {
-      log_vector.erase(log_vector.begin(), log_vector.end());
-
-      log_mtx.unlock();
+      ret = read_at_nolock(index, message);
+      unlock_read();
     }
-    catch (const exception &e)
+    catch (exception &ex)
     {
-      cerr << "Caught exception: " << e.what() << endl;
+      cerr << "Caught exception: " << ex.what() << endl;
 
-      log_mtx.unlock();
+      unlock_read();
+      return false;
     }
+
+    return ret;
+  }
+
+  // Returns success value
+  bool read_all(string& result)
+  {
+    lock_read();
+
+    try
+    {
+      for (string log : log_vector)
+        result = result + log + "\n";
+
+      unlock_read();
+      return true;
+    }
+    catch (exception &ex)
+    {
+      cerr << "Caught exception: " << ex.what() << endl;
+
+      unlock_read();
+      return false;
+    }
+  }
+
+  void clear()
+  {
+    write_mtx.lock();
+
+    try
+    {
+      log_vector.clear();
+      write_mtx.unlock();
+    }
+    catch (exception &ex)
+    {
+      cerr << "Caught exception: " << ex.what() << endl;
+      write_mtx.unlock();
+    }
+
   }
 };
 
-class Buffer {
-  vector<int>
+class Buffer
+{
+  vector<int> buf;
+};
+
+
+void log_test_1_sync() {
+  Logger logger;
+  logger.log("[1] Hello world!");
+  logger.log("[2] How are you doing?");
+  logger.log("[3] three!");
+  logger.log("[4] What why are you still here?");
+
+  string first;
+  bool success1 = logger.read_at(1, first);
+  if (success1)
+    cout << first << endl;
+  else cout << "Failure to read log at index 1" << endl;
+
+  string last;
+  bool success2 = logger.read_last(last);
+  if (success2)
+    cout << last << endl;
+  else cout << "Failure to read log at last index" << endl;
+
+  string all;
+  bool success3 = logger.read_all(all);
+  if (success3)
+    cout << all << endl;
+  else cout << "Failure to read all logs" << endl;
+
+  string fail;
+  bool success4 = logger.read_at(4, fail);
+  if (success4)
+    cout << fail << endl;
+  else cout << "Failure to read log at index 4 (this should fail)." << endl;
 }
 
 int main(int argc, char *argv[])
 {
-  std::cout << "Hello world" << std::endl;
+  log_test_1_sync();
   return 0;
 }
